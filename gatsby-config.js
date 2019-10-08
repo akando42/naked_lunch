987d534730f6f0ecd9b2008c0ad91e59a44dd33e
Troy Do <do@infinitylab.world>
2019-10-07 21:40:33 -0500

    New Description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `E-commerce Snipcart and Gatsby Starter`,
-    author: `Issy Dennis`,
-    description: `A simple e-commerce shop using Gatsby and Snipcart.`,
-    siteUrl: `https://gatsby-snipcart-starter.netlify.com/`,
+    title: `Naked Lunch Studio`,
+    author: `Naked Lunch Group`,
+    description: `Paint Beautify People At Noon`,
+    siteUrl: `https://nakedlunch.live/`,
   },
   plugins: [
     {
